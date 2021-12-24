5f6f9ca25b4844d2ed0a7a6f1f22074e5dc9460a
TanametTpk <40013261+TanametTpk@users.noreply.github.com>
2021-12-24 17:21:29 +0000

    Deploying to gh-pages from  @ 5efe1b51865140cbe9a59a606edbe28f753d7fa0 🚀

---
+++ _next/static/3OOy4EwI62Qw1sTAmsNIZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/605-0325b0d5ca15e651.js","static/chunks/pages/index-5b0fab53c835cca4.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/krvzGxh_VKeM0bJnz6pYv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/605-933f45227d899dee.js","static/chunks/pages/index-5b0fab53c835cca4.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
